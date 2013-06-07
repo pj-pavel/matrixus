41277457f97d153d39bdbc237f39d18e3bfc8611
Pavel Skakun <pj_pavel@mail.ru>
2013-06-07 21:05:35 +0300

    Init commit

---
+++ Matrixus.Web/Controllers/MatrixController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Matrixus.Web.Controllers
+{
+    public class MatrixController : Controller
+    {
+        //
+        // GET: /Matrix/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
